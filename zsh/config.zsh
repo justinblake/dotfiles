08d57daa6a96f5be8ee51728e9000eaad795428a
John Walker <john.walker.dev@gmail.com>
2015-06-17 16:28:02 -0600

    feat(zshrc): debug for tests

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -35,7 +35,11 @@ setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
-
+setopt AUTO_CD
+setopt MULTIOS
+setopt AUTO_NAME_DIRS
+setopt PUSHD_TO_HOME
+setopt RM_STAR_WAIT
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 # setopt complete_aliases

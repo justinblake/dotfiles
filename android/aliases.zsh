0b2810d2fc8f380d11008e9fcc3e990b758a061d
John Walker <john.walker.dev@gmail.com>
2015-07-17 13:20:29 -0600

    chore(all): clean up repo

--- android/aliases.zsh
+++ android/aliases.zsh
@@ -1,7 +1,7 @@
 
 # adb
-#alias remove="adb shell 'su -c \"mount -o rw,remount /system;pm disable com.moki.android.kiosk;pm uninstall com.moki.android.kiosk;rm /system/app/mma.apk /data/app/com.moki.android.kiosk* /data/app-lib/com.moki.android.kiosk /sdcard/MokiMDM;mount -o remount,ro /system    ;sync;reboot\"'"
-#alias install="adb shell 'su -c \"mount -o rw,remount /system;cp /sdcard/mma.apk /system/app/mma.apk;chmod 0666 /system/app/mma.apk;mount -o ro,remount /system;reboot\"'"
+alias remove="adb shell 'su -c \"mount -o rw,remount /system;pm disable com.moki.android.kiosk;pm uninstall com.moki.android.kiosk;rm /system/app/mma.apk /data/app/com.moki.android.kiosk* /data/app-lib/com.moki.android.kiosk /sdcard/MokiMDM;mount -o remount,ro /system    ;sync;reboot\"'"
+alias install="adb shell 'su -c \"mount -o rw,remount /system;cp /sdcard/mma.apk /system/app/mma.apk;chmod 0666 /system/app/mma.apk;mount -o ro,remount /system;reboot\"'"
 alias pidmma='adb shell ps |grep kiosk'
 alias pidmt='adb shell ps |grep com.moki.touch'
 alias pidsdk='adb shell ps |grep com.example.sdktest'

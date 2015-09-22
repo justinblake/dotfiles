038cffbfad1f83b2bac6d21bcf77229fb8d41190
John Walker <john.walker.dev@gmail.com>
2015-09-21 20:36:34 -0600

    test(-z): https://github.com/robbyrussell/oh-my-zsh.git

--- git/functions.zsh
+++ git/functions.zsh
@@ -1,25 +1,37 @@
 
 ff () {
+  if [[ ! "$1"]] ; then
+    return
+  fi
   git add .
   git commit -a -m "feat($1): $2"
 }
 
 fix () {
+  if [[ ! "$1"]] ; then
+    return
+  fi
   git add .
   git commit -a -m "fix($1): $2"
 }
 
 perf () {
+  if [[ ! "$1"]] ; then
+    return
+  fi
   git add .
   git commit -a -m "perf($1): $2"
 }
 
 chore () {
+  if [[ ! "$1"]] ; then
+    return
+  fi
   git add .
   git commit -a -m "chore($1): $2"
 }
 
-test () {
-  git add .
-  git commit -a -m "test($1): $2"
-}
+# test () {
+#   git add .
+#   git commit -a -m "test($1): $2"
+# }

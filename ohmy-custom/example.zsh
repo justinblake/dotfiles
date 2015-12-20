a48ac2fa982ddc1ffc0b37f4b2f5378855038c67
John Walker <john.walker.dev@gmail.com>
2015-12-20 14:53:08 -0700

    perf(ohmy): speed is faster

---
+++ ohmy-custom/example.zsh
@@ -0,0 +1,10 @@
+# Add yourself some shortcuts to projects you often work on
+
+_MANAGER=$DEV/moki/manager
+
+oldmaster=$_MANAGER/manager-master/web
+olduat=$_MANAGER/manager-uat/web
+oldstage=$_MANAGER/manager-stage/web
+
+dev=$_MANAGER/moki-web-dev/web
+master=$_MANAGER/moki-web-master/web
--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -6,3 +6,19 @@ zstyle ':completion:*' insert-tab pending
 
 # Make zsh know about hosts already accessed by SSH
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+
+zstyle ':completion::complete:*' cache-path ~/tmp/.zcache
+zstyle ':completion:*:cd:*' ignore-parents parent pwd
+
+zstyle ':completion:*:match:*' original only
+zstyle ':completion::prefix-1:*' completer _complete
+zstyle ':completion:predict:*' completer _complete
+zstyle ':completion:incremental:*' completer _complete _correct
+zstyle ':completion:*' completer _complete _prefix _correct _prefix _match _approximate
+
+zstyle ':completion:*' expand 'yes'
+zstyle ':completion:*' squeeze-shlashes 'yes'
+zstyle ':completion::complete:*' '\\'
+
+zstyle ':completion:*' menu select
+zstyle ':completion:*:*:default' force-list always
